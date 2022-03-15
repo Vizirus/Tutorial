c6af9ae4d88d317645a1ca44cf92d753fdbf8b99
artem <denloken@gmail.com>
2022-03-15 18:50:12 +0200

    First Commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Threading;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleApp3
+{
+    
+    class Program
+    {
+       class ForJSON
+        {
+            public int i { get; set; } 
+            public int j { get; set; }
+
+            public string str { get; set; }
+            public void Lol()
+            {
+                i = 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            ForJSON forJSON = new ForJSON();
+            string need = JsonSerializer.Serialize(forJSON, options);
+            using(StreamWriter writer = new StreamWriter("test.json", false))
+            {
+                writer.Write(need);
+            }
+
+        }
+
+    }
+    
+
+    
+   
+
+}
